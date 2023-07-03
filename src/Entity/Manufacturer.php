5562536d70a3bde3589b3b9bfee822019fcef0e5
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-07-03 04:58:15 +0200

    add doctrine to entity

--- src/Entity/Manufacturer.php
+++ src/Entity/Manufacturer.php
@@ -5,26 +5,33 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use DateTime;
-use DateTimeInterface;
+use Doctrine\Orm\Mapping as ORM;
 
 /** A manufacturer */
 #[ApiResource]
+#[ORM\Entity]
 class Manufacturer
 {
     /** The ID of the manufacturer */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
     private ?int $id = null;
 
     /** The name of the manufacturer */
+    #[ORM\Column(length: 255)]
     private string $name = '';
 
     /** The description of the manufacturer */
+    #[ORM\Column(length: 255)]
     private string $description = '';
 
     /** The country code of the manufacturer */
+    #[ORM\Column(length: 3)]
     private string $countryCode = '';
 
     /** The date that the manufacturer was listed */
+    #[ORM\Column]
     private ?\DateTimeInterface $listedDate = null;
 
     public function getName(): string
