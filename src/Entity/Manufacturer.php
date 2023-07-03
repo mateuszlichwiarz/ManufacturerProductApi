2a5c0cc74a56f6dddc84249641b066eb296ed2ff
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-07-03 04:42:14 +0200

    add manufacturer properties

--- src/Entity/Manufacturer.php
+++ src/Entity/Manufacturer.php
@@ -5,14 +5,69 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use DateTime;
+use DateTimeInterface;
 
 /** A manufacturer */
 #[ApiResource]
 class Manufacturer
 {
-    /** ID of the manufacturer */
+    /** The ID of the manufacturer */
     private ?int $id = null;
 
-    /** A name of the manufacturer */
-    public string $name = '';
+    /** The name of the manufacturer */
+    private string $name = '';
+
+    /** The description of the manufacturer */
+    private string $description = '';
+
+    /** The country code of the manufacturer */
+    private string $countryCode = '';
+
+    /** The date that the manufacturer was listed */
+    private ?\DateTimeInterface $listedDate = null;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getCountryCode(): string
+    {
+        return $this->countryCode;
+    }
+
+    public function setCountryCode(string $countryCode): void
+    {
+        $this->countryCode = $countryCode;
+    }
+
+    public function getListedDate(): ?\DateTimeInterface
+    {
+        return $this->listedDate;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $listedDate
+     */
+    public function setListedDate(?\DateTimeInterface $listedDate): void
+    {
+        $this->listedDate = $listedDate;
+    }
+
 }
