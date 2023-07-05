5bc469b10c2e5cb4e790288fc88d38bcf893aba5
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-07-05 02:45:57 +0200

    add orm attribute to entity

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -5,21 +5,31 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
 
 use App\Entity\Manufacturer;
 
 #[ApiResource]
+#[ORM\Entity]
 class Product
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column]
     private ?string $mpn = null;
 
+    #[ORM\Column(length: 255)]
     private string $name = '';
 
+    #[ORM\Column(type: "text")]
     private string $description = '';
 
+    #[ORM\Column(type: "datetime")]
     private ?\DateTimeInterface $issueDate = null;
 
+    #[ORM\ManyToOne(targetEntity: Manufacturer::class)]
     private ?Manufacturer $manufacturer;
 }
