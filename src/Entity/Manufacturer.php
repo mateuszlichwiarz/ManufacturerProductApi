be774fd3c65ad80cef6725a205aa28ba26e35050
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-07-06 04:50:29 +0200

    set pagination to Manufacturer Entity

--- src/Entity/Manufacturer.php
+++ src/Entity/Manufacturer.php
@@ -16,12 +16,12 @@ use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
-
 use Symfony\Component\Validator\Constraints as Assert;
 
 /** A manufacturer */
 #[ORM\Entity]
 #[ApiResource(
+    paginationItemsPerPage: 5,
     operations: [
         new Get(),
         new GetCollection(),
