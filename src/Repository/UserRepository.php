a11ed9e43f8ba754065a7331d43bccc30a14ea47
mateusz_lichwiarz <mateusz.lichwiarz@gmail.com>
2023-07-12 03:14:41 +0200

    add find method

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -9,6 +9,8 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
+use App\Repository\ApiTokenRepository;
+
 /**
  * @extends ServiceEntityRepository<User>
  *
@@ -19,9 +21,14 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private  ApiTokenRepository $apiTokenRepository)
     {
         parent::__construct($registry, User::class);
+    }
+
+    public function findByApiToken(string $apiToken): ?User
+    {
+        return $this->apiTokenRepository->findOneBy(['token' => $apiToken])?->getUserOwner();
     }
 
     public function save(User $entity, bool $flush = false): void
